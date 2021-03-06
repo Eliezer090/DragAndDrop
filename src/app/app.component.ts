14bc7f838e76ff7c2870571844071d6a9352e8c5
Eliezer <eliezer.mail090@gmail.com>
2021-03-06 19:26:14 -0300

    Commit Objects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { fromEvent } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Drag';
+   
+  @ViewChild('myrect',{static: true}) myrect!: ElementRef;
+  top: number = 40;
+  left:number = 40;
+  constructor() { }
+
+  ngOnInit(): void {
+    let mouseDown = fromEvent<MouseEvent>(this.myrect.nativeElement, 'mousedown');
+    let mouseMove = fromEvent<MouseEvent>(document,'mousemove');
+    let mouseUp = fromEvent<MouseEvent>(document,'mouseup');
+
+    mouseDown.subscribe((e:MouseEvent)=> {
+      //console.log(e);
+      let x= e.screenX;
+      let y= e.screenY;
+      
+      mouseMove
+      //takeUntil tem a função de dizer "ok agora nao de mais o sub nesse mousemove e de no mouseUp"
+      .pipe(takeUntil(mouseUp))
+      .subscribe((em: MouseEvent)=>{
+        //console.log(em);
+        let offsetX= x -em.screenX;
+        let offsetY= y -em.screenY;
+        this.top-=offsetY;
+        this.left-=offsetX;
+        x = em.screenX;
+        y = em.screenY;
+      });
+
+    })
+  }
 }
